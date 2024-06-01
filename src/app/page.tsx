32822775144f7c5fbc9cdac4442542b7b0f93c40
lidiaCana <in.canaless@gmail.com>
2024-06-01 19:18:45 -0400

    docs: added template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
+import Video from "@/components/Video";
 import Image from "next/image";
+import Blog from "./blog/page";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="">Coming soon</h1>
-    </main>
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+      <Blog />
+      <Contact />
+    </>
   );
 }
